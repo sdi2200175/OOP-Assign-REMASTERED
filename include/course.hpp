57fa17626a364c6ce68d43874a15db2eeeec181a
sdi2200154 <sdi2200154@di.uoa.gr>
2024-01-17 16:33:20 +0200

    course.hpp + course.cpp

--- include/course.hpp
+++ include/course.hpp
@@ -22,4 +22,29 @@ class Course {
         bool Mandatory;
         unsigned short ECTs;
 
+        // const string CourseID 
+    
+    public:
+
+        /* - Constructors and Destructors - */
+        Course();
+        Course(string Name, bool Mandatory, unsigned short ECTs);
+        ~Course();
+
+        /* - Getters - */
+        inline const string &getName() const { return Name; }
+        inline const bool &getMandatory() const { return Mandatory; }
+        inline const unsigned short &getECTs() const { return ECTs; }
+
+        // inline const string &getCourseID() const { return CourseID; }
+
+        /* - Setters - */
+        inline void setName(string Name) { this->Name = Name; }
+        inline void setMandatory(bool Mandatory) { this->Mandatory = Mandatory; }
+        inline void setECTs(unsigned short ECTs) { this->ECTs = ECTs; }
+
+        /* - Operator Overloads - */
+        friend ostream &operator<< (ostream &str, Course &course);
+        friend istream &operator>> (istream &str, Course &course);
+
 };
--- source/course.cpp
+++ source/course.cpp
@@ -1,6 +1,6 @@
 /**
  * @file course.hpp
- * @author Spyros Strakosia
+ * @author Spyros Strakosia, Evaggelia Ragkousi
  * @brief 
  * @version 
  * @date 2024-01-11
@@ -9,9 +9,39 @@
  * 
  */
 
-#pragma once
-
-#include <iostream>
 #include "course.hpp"
 
-using namespace std;
+Course::Course() {
+	cin >> *this;
+}
+
+Course::Course(string Name, bool Mandatory, unsigned short ECTs) : Name(Name), Mandatory(Mandatory), ECTs(ECTs) {
+	cout << "|" << endl << "+--------- Constructing Course --------+" << endl;
+	cout << "+--------- Constructed Course ---------+" << endl;	
+}
+
+Course::~Course() {}
+
+ostream &operator<< (ostream &str, Course &course) {
+	str << "|" << endl << "+---------- Course Properties ---------+" << endl;
+	str << "| Name: " << course.Name << endl;
+	if (course.Mandatory) { str << "| The course is Mandatory: " << endl; }
+	else { str << "| The course is not Mandatory: " << endl; }
+    str << "| ECTs: " << course.ECTs << endl;
+	str << "+-------- End of Course Details -------+" << endl << "|" << endl;
+	return str;
+}
+
+istream &operator>> (istream &str, Course &course) {
+	string Buffer;
+	cout << "> Enter Name: ";
+	getline(str, course.Name);
+    cout << "> Is the course Mandatory? (Y/N)";
+    getline(str, Buffer);
+    if (Buffer.compare("Y") || Buffer.compare("y")) { course.Mandatory = true; }
+    else { course.Mandatory = false; }
+	cout << "> Enter ECTs: ";
+	getline(str, Buffer);
+	course.ECTs = (unsigned short)stoi(Buffer);
+	return str;
+}
